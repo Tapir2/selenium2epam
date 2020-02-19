290ed3d0dea51175d5b9088f0646d505e882ecaf
Yury Rubis <yury_rubis@epam.com>
2020-02-19 16:03:21 +0300

    Add assertion with TestNG

--- src/test/java/DemoFormTest.java
+++ src/test/java/DemoFormTest.java
@@ -1,19 +1,41 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 public class DemoFormTest {
 
+    public static final String TEST_STRING = "Hello";
+
     @Test
-    public void testMessage () {
+    public void testMessage () throws Throwable {
 
         System.setProperty("webdriver.gecko.driver", "drivers/geckodriver.exe");
 
         WebDriver driver = new FirefoxDriver();
 
-        driver.get("https://www.seleniumeasy.com/test/basic-first-form-demo.html");
+        try {
+            driver.get("https://www.seleniumeasy.com/test/basic-first-form-demo.html");
 
-        driver.quit();
+            WebElement inputTextField = driver.findElement(By.id("user-message"));
+
+            inputTextField.sendKeys(TEST_STRING);
+
+            WebElement showMessageButton = driver.findElement(By.xpath("//button[contains(text(),'Show Message')]"));
+
+            showMessageButton.click();
+
+            WebElement outTextField = driver.findElement(By.id("display"));
+
+            String outputMessage = outTextField.getText();
+
+            assert TEST_STRING.equalsIgnoreCase(outputMessage);
+        } catch (Throwable t) {
+            throw t;
+        } finally {
+            driver.quit();
+        }
     }
 
     @Test
