965ab9a9283625d576bc3b8ac4055fac8d5d44dc
Yury Rubis <yury_rubis@epam.com>
2020-02-19 15:20:11 +0300

    Add geckodriver.exe, add first test with TestNG

--- src/test/java/DemoFormTest.java
+++ src/test/java/DemoFormTest.java
@@ -1,6 +1,23 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
 public class DemoFormTest {
 
+    @Test
     public void testMessage () {
+
+        System.setProperty("webdriver.gecko.driver", "drivers/geckodriver.exe");
+
+        WebDriver driver = new FirefoxDriver();
+
+        driver.get("https://www.seleniumeasy.com/test/basic-first-form-demo.html");
+
+        driver.quit();
+    }
+
+    @Test
+    public void testMessage2 () {
 
     }
 }
