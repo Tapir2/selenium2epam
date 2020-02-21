8282d913231e9d1da2040a54d8e847714a9d3bdc
Yury Rubis <yury_rubis@epam.com>
2020-02-21 11:58:28 +0300

    Add screenshots capture

--- src/test/java/BasicTest.java
+++ src/test/java/BasicTest.java
@@ -1,9 +1,19 @@
+import org.apache.commons.io.FileUtils;
+import org.apache.log4j.Logger;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
+import java.io.File;
+import java.io.IOException;
+
 abstract public class BasicTest {
+
+    private static final Logger LOGGER = Logger.getLogger(BasicTest.class);
     protected WebDriver driver;
 
     @BeforeMethod
@@ -14,8 +24,21 @@ abstract public class BasicTest {
     }
 
     @AfterMethod
-    protected void close () {
+    protected void close (ITestResult testResult) {
+        
+        makeScreenshot(testResult);
 
         driver.quit();
+    }
+
+    private void makeScreenshot(ITestResult testResult) {
+        if (!testResult.isSuccess()) {
+            File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+            try {
+                FileUtils.moveFile(screenshot, new File("c:/Git/selenium2epam/screenshots/" + testResult.getName() + ".png"));
+            } catch (IOException e) {
+                LOGGER.error(e.getMessage());
+            }
+        }
     }
 }
--- src/test/java/CheckboxTest.java
+++ src/test/java/CheckboxTest.java
@@ -1,10 +1,7 @@
 import org.apache.log4j.Logger;
 import org.assertj.core.api.Assertions;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 import pages.CheckboxPage;
-import pages.DemoFormPage;
 
 public class CheckboxTest extends BasicTest {
 
