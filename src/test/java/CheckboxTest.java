1ba05570c22dd65301ded308938ef47c903b68f8
Yury Rubis <yury_rubis@epam.com>
2020-02-21 11:07:25 +0300

    Add logging with log4j

--- src/test/java/CheckboxTest.java
+++ src/test/java/CheckboxTest.java
@@ -1,3 +1,4 @@
+import org.apache.log4j.Logger;
 import org.assertj.core.api.Assertions;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
@@ -6,8 +7,13 @@ import pages.CheckboxPage;
 import pages.DemoFormPage;
 
 public class CheckboxTest extends BasicTest {
+
+    private static final Logger LOGGER = Logger.getLogger(CheckboxTest.class);
+
     @Test
     public void singleCheckboxTest () {
+
+        LOGGER.info("Invoke singleCheckboxTest");
 
         String outputMessage = new CheckboxPage(driver).singleCheckboxForm();
 
